ceb1f7e0f514e240e802f6d2561e80ab626e35b2
josenuwe <jose@nuwe.io>
2025-03-09 04:26:27 +0100

    Add: challenge template

---
+++ src/test/java/com/round3/realestate/RealstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.round3.realestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
