c36196b9a7daee6836128eee5930398390f3b718
jraporta <jraporta@gmail.com>
2025-03-21 09:48:07 +0100

    first commit

---
+++ src/main/java/com/round3/realestate/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.round3.realestate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/health").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement((session -> session.sessionCreationPolicy((SessionCreationPolicy.STATELESS))))
+                .build();
+    }
+
+}
